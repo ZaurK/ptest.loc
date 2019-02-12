8d3b9951380f6653db34ff981a0521c4241b498f
ZaurK <zaurpost@yandex.ru>
2019-02-12 23:59:56 +0300

    step21

--- controllers/DefaultController.php
+++ controllers/DefaultController.php
@@ -22,13 +22,21 @@ class DefaultController extends \yii\web\Controller
             'class' => 'vova07\imperavi\actions\GetImagesAction',
             'url' => 'http://localhost:8080/img/uploads/', // Directory URL address, where files are stored.
             'path' => '@app/web/img/uploads', // Or absolute path to directory where files are stored.
-            'options' => ['only' => ['*.jpg', '*.jpeg', '*.png', '*.gif', '*.ico']], // These options are by default.
+            'options' => ['only' => ['*.jpg', '*.jpeg', '*.JPG', '*.JPEG', '*.png', '*.PNG', '*.gif', '*.ico']], // These options are by default.
         ],
+        'file-delete' => [
+           'class' => 'vova07\imperavi\actions\DeleteFileAction',
+           'url' => 'http://localhost:8080/img/uploads/', // Directory URL address, where files are stored.
+          'path' => '@app/web/img/uploads', // Or absolute path to directory where files are stored.
+       ],
         'file-upload' => [
            'class' => 'vova07\imperavi\actions\UploadFileAction',
            'url' => 'http://localhost:8080/files/', // Directory URL address, where files are stored.
            'path' => '@app/web/files', // Or absolute path to directory where files are stored.
            'uploadOnlyImage' => false, // For any kind of files uploading.
+           'unique' => false,
+           'replace' => true, // By default it throw an excepiton instead.
+           'translit' => true,
        ],
         'files-get' => [
            'class' => 'vova07\imperavi\actions\GetFilesAction',
@@ -36,6 +44,7 @@ class DefaultController extends \yii\web\Controller
            'path' => '@app/web/files', // Or absolute path to directory where files are stored.
            'options' => ['only' => ['*.pdf', '*.doc', '*.docx', '*.xls', '*.xlsx', '*.txt', '*.md']], // These options are by default.
        ],
+
     ];
 }
 
